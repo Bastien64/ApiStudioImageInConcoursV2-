b6fb28dc611c8c45fdbff1d7ec1ab46d7cc098dc
Bastien VILLEGAS <95182085+Bastien64@users.noreply.github.com>
2023-11-12 10:43:47 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, jsonify, request,make_response
+from flask import Flask, jsonify, request, make_response, current_app
 from flask_mysqldb import MySQL
 from werkzeug.middleware.proxy_fix import ProxyFix
 from flask_cors import CORS
@@ -9,19 +9,20 @@ import io
 from io import BytesIO
 from PIL import Image
 import csv
+import os  
 
-
-
+WSGIRequestHandler.protocol_version = "HTTP/1.1"
 app = Flask(__name__)
-
+port = int(os.environ.get('PORT', 5000))
 # Configuration de la base de données sur CONCOURS PHOTO
 CORS(app, resources={r"/*": {"origins": "*"}})
-app.config['MYSQL_HOST'] = '79.137.81.61'
+app.config['MYSQL_HOST'] = '162.19.67.246'
 app.config['MYSQL_USER'] = 'admin'
 app.config['MYSQL_PASSWORD'] = '123456Azerty!'
-app.config['MYSQL_DB'] = 'studiophoto'
+app.config['MYSQL_DB'] = 'studiophotov2'
 
 mysql = MySQL(app)
+
 
 @app.route('/admin', methods=['GET'])
 def get_admins():
@@ -54,6 +55,20 @@ def get_votants():
         votants.append(votant_data)
     return jsonify(votants)
 
+@app.route('/categorie', methods=['GET'])
+def get_categorie():
+    cur = mysql.connection.cursor()
+    cur.execute('''SELECT * FROM categorie''')
+    result = cur.fetchall()
+    categories = []
+    for categorie in result:
+        categorie_data = {}
+        categorie_data['id'] = categorie[0]
+        categorie_data['Type'] = categorie[1]
+        categories.append(categorie_data)
+    return jsonify(categories)
+
+
 @app.route('/votant', methods=['POST'])
 def add_votant():
     Nom = request.json['Nom']
@@ -78,7 +93,7 @@ def delete_all_votant():
 @app.route('/photo', methods=['GET'])
 def get_photos():
     cur = mysql.connection.cursor()
-    cur.execute('''SELECT * FROM photo''')
+    cur.execute('''SELECT * FROM photoconcours''')
     result = cur.fetchall()
     photos = []
     for photo in result:
@@ -86,38 +101,42 @@ def get_photos():
         photo_data['id'] = photo[0]
         photo_data['Nom'] = photo[1]
         photo_data['Prenom'] = photo[2]
-        image_binary_data = photo[3]
+        photo_data['Email'] = photo[3]
+        photo_data['Telephone'] = photo[4]
+        image_binary_data = photo[5]
         image = Image.open(BytesIO(image_binary_data))
         image_buffer = BytesIO()
         image.save(image_buffer, format=image.format)
-        photo_data['Link'] = base64.b64encode(image_buffer.getvalue()).decode('utf-8')
-        photo_data['Vote'] = photo[4]
+        photo_data['Photo'] = base64.b64encode(image_buffer.getvalue()).decode('utf-8')
+        photo_data['Vote'] = photo[6]
+        photo_data['Categorie_id'] = photo[7]
         photos.append(photo_data)
     return jsonify(photos)
 
-
 @app.route('/photo', methods=['POST'])
 def add_photo():
-    prenom = request.form['Prenom']
     nom = request.form['Nom']
-    file = request.files['Image']
+    prenom = request.form['Prenom']
+    email = request.form['Email']
+    telephone = request.form['Telephone']
+    file = request.files['Link']
     
     # Ouvrir l'image avec Pillow
     img = Image.open(file)
     
     # Redimensionner l'image en largeur maximale de 100 pixels et hauteur automatique
-    max_width = 100
+    max_width = 500
     width_percent = (max_width / float(img.size[0]))
     new_height = int((float(img.size[1]) * float(width_percent)))
     img = img.resize((max_width, new_height), Image.LANCZOS)
     
     # Compression JPEG avec une qualité de 85 (vous pouvez ajuster la qualité selon vos besoins)
-    jpeg_quality = 85
+    jpeg_quality = 100
     img_byte_array = BytesIO()
     img.save(img_byte_array, format='JPEG', quality=jpeg_quality, optimize=True)
     
     # Réduire la taille du fichier pour ne pas dépasser 200 Ko
-    while img_byte_array.tell() > 200 * 1024:  # 200 Ko en octets
+    while img_byte_array.tell() > 400 * 1024:  # 200 Ko en octets
         jpeg_quality -= 5
         img_byte_array = BytesIO()
         img.save(img_byte_array, format='JPEG', quality=jpeg_quality, optimize=True)
@@ -125,20 +144,30 @@ def add_photo():
     file_read = img_byte_array.getvalue()
     
     cur = mysql.connection.cursor()
-    cur.execute("INSERT INTO photo (Prenom, Nom, Link, Vote) VALUES (%s, %s, %s, %s)", (prenom, nom, file_read, 0))
+    cur.execute("INSERT INTO photoconcours (Nom, Prenom, Email, Telephone, Link, Vote, Categorie_id) VALUES (%s, %s, %s, %s, %s, %s, %s)", (nom, prenom, email, telephone, file_read, 0, 2))
     mysql.connection.commit()
     cur.close()
     return 'success'
 
+@app.route('/photo/update_categorie/<int:photo_id>', methods=['POST'])
+def update_photo_categorie(photo_id):
+    try:
+        nouvelle_categorie_id = request.json['nouvelle_categorie_id']
+        cur = mysql.connection.cursor()
+        cur.execute('UPDATE photoconcours SET Categorie_id = %s WHERE id = %s', (nouvelle_categorie_id, photo_id))
+        mysql.connection.commit()
+        return jsonify({'message': 'Categorie_id modifié avec succès'})
+    except Exception as e:
+        return jsonify({'error': str(e)}, 500)
 
 @app.route('/photo/<int:photo_id>/vote', methods=['PUT'])
 def update_photo_vote(photo_id):
     cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM photo WHERE id=%s", (photo_id,))
+    cur.execute("SELECT * FROM photoconcours WHERE id=%s", (photo_id,))
     photo = cur.fetchone()
     if not photo:
         return jsonify({'error': 'Photo not found'}), 404
-    cur.execute("UPDATE photo SET Vote=%s WHERE id=%s", (photo[4] + 1, photo_id))
+    cur.execute("UPDATE photoconcours SET Vote=%s WHERE id=%s", (photo[6] + 1, photo_id))
     mysql.connection.commit()
     cur.close()
     return jsonify({'message': 'Vote updated successfully'})
@@ -178,6 +207,38 @@ def download_admins():
     output.headers["Content-type"] = "text/csv"
     return output
 
+@app.route('/date', methods=['GET'])
+def get_date():
+    cur = mysql.connection.cursor()
+    cur.execute('''SELECT * FROM dateconcours''')
+    result = cur.fetchall()
+    dates = []
+    for date in result:
+        date_data = {}
+        date_data['id'] = date[0]
+        date_data['Datedebut'] = date[1]
+        date_data['Datedefin'] = date[2]
+        dates.append(date_data)
+    return jsonify(dates)
+
+@app.route('/date', methods=['DELETE'])
+def delete_all_dates():
+    cur = mysql.connection.cursor()
+    cur.execute('DELETE FROM dateconcours')
+    mysql.connection.commit()
+    return jsonify({'message': 'Toutes les dates ont été supprimées avec succès'})
+
+@app.route('/date', methods=['POST'])
+def add_date():
+    Datedebut = request.json['Datedebut']
+    Datedefin = request.json['Datedefin']
+    cur = mysql.connection.cursor()
+    cur.execute('''INSERT INTO dateconcours (Datedebut, Datedefin) VALUES(%s, %s)''',
+                (Datedebut, Datedefin))
+    mysql.connection.commit()
+    return jsonify({'message': 'date added successfully'})
+
 if __name__ == '__main__':
-    app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
-    app.run(host='0.0.0.0', port=5000)
+    # Utilisez os.environ.get pour obtenir le port depuis la variable d'environnement
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
